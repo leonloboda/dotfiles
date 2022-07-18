18fad1d77b2f232b183b7fd3f90e6d39888309ac
leonloboda <66636996+leonloboda@users.noreply.github.com>
2022-07-18 10:50:12 +0200

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env sh
+
+# Configure monitors
+autorandr --change
+
+polybar mybar &
 
 # Terminate already running bar instances
 killall -q polybar
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar mybar &
+# Wait until the processes have been shut down
+while pgrep -x polybar > /dev/null ; do sleep 1 ; done
 
+# Launch bars
+monitors="$(polybar -m)"
 
-echo "Polybar launched..."
+echo "${monitors}" | while read -r monitor; do
+    output="$(echo "${monitor}" | cut -d ':' -f 1)"
+    position="$(echo "${monitor}" | cut -d '+' -f 2)"
+    bar="mybar"
+
+    if [[ $(echo "${monitors}" | wc -l) -gt 1 ]]; then
+        [[ $(echo "${position}") -eq 0 ]] && bar="left" || bar="right"
+    fi
+
+    MONITOR=${output} polybar -l error -c ~/.config/polybar/config.ini "${bar}" &
+done
